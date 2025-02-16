05cb3e14116d9deb767b4fd5a4b4f05bac16589f
Tech-xpat <empiredigitalsworldwide@gmail.com>
2025-02-16 17:28:24 +0100

    Update page.tsx

--- My Project/app/contact/page.tsx
+++ My Project/app/contact/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { motion } from "framer-motion"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faEnvelope, faPhone, faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons"
